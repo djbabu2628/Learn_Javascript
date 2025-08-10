cd7e1c5c723a44fa2348bd6d08a6ebc4bb107233
My-Hospital <viveksunilchavhan@gmail.com>
2025-08-10 15:21:18 +0530

    Error Handling

---
+++ error_handling.js
@@ -0,0 +1,14 @@
+let a = 5, b = 5;
+console.log("a =",a);
+console.log("b =",b);
+console.log("a+b =",a+b);
+console.log("a*b =",a*b);
+console.log("a-b =",a-b);
+try{
+    console.log("a/b =",a/c);
+}
+catch(err) {
+    console.log(err);
+}
+
+console.log("a%b =",a%b);
