224be659d269d9e243de4c2812667eddb62f231b
My-Hospital <viveksunilchavhan@gmail.com>
2025-08-12 18:00:56 +0530

    callback hell

--- callback_hell.js
+++ callback_hell.js
@@ -1,5 +1,20 @@
-function getData(dataID) {
+function getData(dataID, getNextdata) {
     setTimeout( () => {
         console.log("hello",dataID);
+        if(getNextdata){
+            getNextdata();
+        }
     },2000);
 }
+
+
+getData(1, () => {
+    console.log("Getting Data.........");
+    getData(2, () => {
+            console.log("Getting Data.........");
+        getData(3, ()=> {
+                console.log("Getting Data.........");
+            getData("End");
+        })
+    });
+})
