8307c3fe5bb44ef0a4494ba4d8d5cc648a05c9c5
My-Hospital <viveksunilchavhan@gmail.com>
2025-08-12 18:53:38 +0530

    promises

---
+++ promises.js
@@ -0,0 +1,24 @@
+
+
+function getData(data, getNxtData)  {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+        console.log("Data",data);
+        console.log("I am promise");
+        resolve("Success");
+        if (getNxtData){
+            getNxtData();
+        }
+        },5000);
+});
+};
+
+function geterror(d){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Data",d);
+            reject("error");
+        },6000)
+    });
+};
+
