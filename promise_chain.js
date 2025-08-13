de968a2ed8cc69ff2863b790a3d1fdc32807137f
My-Hospital <viveksunilchavhan@gmail.com>
2025-08-13 17:44:46 +0530

    promise chain in simple way and easy understandable code

--- promise_chain.js
+++ promise_chain.js
@@ -1,28 +1,48 @@
-function asyncfun1()  {
-    return new Promise((resolve, reject) =>{
-        setTimeout(()=>{
+function asyncfun1() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
             console.log("Data 1");
             resolve("Success");
-        },5000);
+        }, 2000);
     });
 };
 
-function asyncfun2(){
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>{
+function asyncfun2() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
             console.log("Data 2");
             resolve("Success");
-        },5000);
+        }, 2000);
     });
 };
 
-console.log("Fetching Data.......");
-let d1 = asyncfun1();
-d1.then((res)=>{
+function getData(data) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Data is: ", data);
+            resolve("Success");
+        }, 4000);
+    });
+};
+
+console.log("Fetching Data........");
+asyncfun1().then((res) => {
     console.log(res);
-    console.log("Fetching Data");
-    let d2 = asyncfun2();
-    d2.then((res)=>{
+    console.log("Fetching Data........");
+    asyncfun2().then((res) => {
         console.log(res);
-    })
-})
+        console.log("Fetching Data........")
+        getData(5).then((res) => {
+            console.log(res);
+            console.log("Fetching Data........")
+            getData(3).then((res) => {
+                
+                console.log(res);
+            });
+        });
+    });
+});
+
+
+
+
