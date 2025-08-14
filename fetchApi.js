da508a1ba07731bddedb7db4c9fdec5aeb4731d7
My-Hospital <viveksunilchavhan@gmail.com>
2025-08-14 18:46:46 +0530

    add button to get API data

--- fetchApi.js
+++ fetchApi.js
@@ -1,13 +1,15 @@
 const URL = "https://catfact.ninja/facts";
-let para = document.querySelector("#para");
+const para = document.querySelector("#para");
+const btn = document.querySelector("#btn");
 
-const getFacts = async ()=>{
+const getFacts = async () => {
     console.log('Getting Data..........');
     let response = await fetch(URL);
     console.log(response);
     let data = await response.json();
     // console.log(data)
     // console.log(data.data[0].fact);
-    para.innerText = data.data[0].fact;
+    para.innerText = data.data[2].fact;
 };
 
+btn.addEventListener("click", getFacts);
