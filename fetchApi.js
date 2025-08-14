ef908b83a559bd8073695dd755c62b11105c9822
My-Hospital <viveksunilchavhan@gmail.com>
2025-08-14 18:39:40 +0530

    print fact on page using p tag

--- fetchApi.js
+++ fetchApi.js
@@ -1,8 +1,13 @@
 const URL = "https://catfact.ninja/facts";
+let para = document.querySelector("#para");
 
 const getFacts = async ()=>{
     console.log('Getting Data..........');
     let response = await fetch(URL);
     console.log(response);
+    let data = await response.json();
+    // console.log(data)
+    // console.log(data.data[0].fact);
+    para.innerText = data.data[0].fact;
 };
 
